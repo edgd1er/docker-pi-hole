24f8765d6eab2053adb19b00b3093c9e5f943250
edgd1er <edgd1er@hotmail.com>
2020-10-21 00:24:12 +0200

    ignore idea folder
    access and error log are fifos
    
    ignore local scripts

--- s6/debian-root/etc/cont-init.d/10-create-fifo.sh
+++ s6/debian-root/etc/cont-init.d/10-create-fifo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/execlineb -P
 
 #lighthttpd cannot use /dev/stdout https://redmine.lighttpd.net/issues/2731
-if { s6-rmrf /var/run/s6/lighttpd-access-log-fifo }
-if { s6-mkfifo /var/run/s6/lighttpd-access-log-fifo }
-if { s6-rmrf /var/run/s6/lighttpd-error-log-fifo }
-if { s6-mkfifo /var/run/s6/lighttpd-error-log-fifo }
+if { s6-rmrf /var/log/lighttpd/access.log }
+if { s6-mkfifo /var/log/lighttpd/access.log }
+if { s6-rmrf /var/log/lighttpd/error.log }
+if { s6-mkfifo /var/log/lighttpd/error.log }
