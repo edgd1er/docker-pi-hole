7703440b16682bd881e789afdb7ca88a075570b2
edgd1er <edgd1er@hotmail.com>
2020-10-22 07:24:51 +0200

    reformat

--- s6/debian-root/etc/cont-init.d/20-start.sh
+++ s6/debian-root/etc/cont-init.d/20-start.sh
@@ -2,22 +2,22 @@
 set -e
 
 bashCmd='bash -e'
-if [ "${PH_VERBOSE:-0}" -gt 0 ] ; then 
-    set -x ;
-    bashCmd='bash -e -x'
+if [ "${PH_VERBOSE:-0}" -gt 0 ]; then
+  set -x
+  bashCmd='bash -e -x'
 fi
 
 # used to start dnsmasq here for gravity to use...now that conflicts port 53
 
 $bashCmd /start.sh
 # Gotta go fast, no time for gravity
-if [ -n "$PYTEST" ]; then 
-    sed -i 's/^gravity_spinup$/#gravity_spinup # DISABLED FOR PYTEST/g' "$(which gravity.sh)" 
+if [ -n "$PYTEST" ]; then
+  sed -i 's/^gravity_spinup$/#gravity_spinup # DISABLED FOR PYTEST/g' "$(which gravity.sh)"
 fi
 if [ -z "$SKIPGRAVITYONBOOT" ]; then
-    gravity.sh
+  gravity.sh
 else
-    echo "  Skipping Gravity Database Update."
+  echo "  Skipping Gravity Database Update."
 fi
 
 # Kill dnsmasq because s6 won't like it if it's running when s6 services start
