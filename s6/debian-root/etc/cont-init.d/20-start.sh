860211cef4a9ecaffd8341345dcb04bd44a5607a
edgd1er <edgd1er@hotmail.com>
2020-10-22 10:32:04 +0200

    GH actions buildx multi arch
    fix secret vars
    
    reformat
    
    move ARCH detection logic inside container scripts
    
    change base image: plain debian-slim
    
    don t push to pihole repository....
    
    build using buildx ( multi arch images )
    
    fix github user for vulnerability scanner
    
    add info

--- s6/debian-root/etc/cont-init.d/20-start.sh
+++ s6/debian-root/etc/cont-init.d/20-start.sh
@@ -2,9 +2,9 @@
 set -e
 
 bashCmd='bash -e'
-if [ "${PH_VERBOSE:-0}" -gt 0 ] ; then
-    set -x ;
-    bashCmd='bash -e -x'
+if [ "${PH_VERBOSE:-0}" -gt 0 ]; then
+  set -x
+  bashCmd='bash -e -x'
 fi
 
 # used to start dnsmasq here for gravity to use...now that conflicts port 53
