219fc22ac27434a7e141c4ef074d2cfb906ab080
edgd1er <edgd1er@hotmail.com>
2022-02-12 23:39:44 +0100

    docker compose 2, bullseye, buildx, maintenance,

--- gh-actions-test.sh
+++ gh-actions-test.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-set -ex
+set -x
 
 # Script ran by Github actions for tests
 #
 # @environment ${ARCH}              The architecture to build. Example: amd64.
-# @environment ${DEBIAN_VERSION}    Debian version to build. ('buster' or 'stretch').
-# @environment ${ARCH_IMAGE}        What the Docker Hub Image should be tagged as. Example: pihole/pihole:master-amd64-buster
+# @environment ${DEBIAN_VERSION}    Debian version to build. ('bullseye' or 'buster').
+# @environment ${ARCH_IMAGE}        What the Docker Hub Image should be tagged as. Example: pihole/pihole:master-amd64-bullseye
 
 # setup qemu/variables
 docker run --rm --privileged multiarch/qemu-user-static:register --reset > /dev/null
@@ -16,7 +16,7 @@ if [[ "$1" == "enter" ]]; then
 fi
 
 # generate and build dockerfile
-docker build --tag image_pipenv --file Dockerfile_build .
+docker build  --tag image_pipenv --file Dockerfile_build .
 docker run --rm \
     --volume /var/run/docker.sock:/var/run/docker.sock \
     --volume "$(pwd):/$(pwd)" \
@@ -29,6 +29,7 @@ docker run --rm \
     --env CORE_VERSION="${CORE_VERSION}" \
     --env WEB_VERSION="${WEB_VERSION}" \
     --env FTL_VERSION="${FTL_VERSION}" \
+    --env S6_OVERLAY_VERSION="${S6_OVERLAY_VERSION}" \
     ${enter} image_pipenv
 
 mkdir -p ".gh-workspace/${DEBIAN_VERSION}/"
