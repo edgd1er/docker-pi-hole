bc59ad58c6255bd5a16f970e8e9ea73d3ac42acd
edgd1er <edgd1er@hotmail.com>
2022-08-30 21:28:02 +0200

    docker compose 2.10.2 for tests

--- test/tests/test_bash_functions.py
+++ test/tests/test_bash_functions.py
@@ -98,7 +98,9 @@ def test_overrides_default_custom_cache_size(docker, slow, test_args, cache_size
     """Changes the cache_size setting to increase or decrease the cache size for dnsmasq"""
     CONFIG_LINE = r"cache-size\s*=\s*{}".format(cache_size)
 
-    function = docker.run('echo ${CUSTOM_CACHE_SIZE};. /usr/local/bin/bash_functions.sh; echo ${CUSTOM_CACHE_SIZE}; eval `grep setup_FTL_CacheSize /usr/local/bin/_startup.sh`')
+    function = docker.run(
+        "echo ${CUSTOM_CACHE_SIZE};. /usr/local/bin/bash_functions.sh; echo ${CUSTOM_CACHE_SIZE}; eval `grep setup_FTL_CacheSize /usr/local/bin/_startup.sh`"
+    )
     assert "Custom CUSTOM_CACHE_SIZE set to {}".format(cache_size) in function.stdout
     slow(
         lambda: re.search(CONFIG_LINE, docker.run(_cat(DNSMASQ_CONFIG_LOC)).stdout)
