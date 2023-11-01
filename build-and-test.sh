a76fce19cd4a43e711e1a478df252f55c7b06daa
edgd1er <3601462+edgd1er@users.noreply.github.com>
2023-11-01 21:09:11 +0100

    fork changes

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,24 +1,57 @@
 #!/usr/bin/env bash
-set -ex
+set -e -u -o pipefail
+set -x
 
-if [[ "$1" == "enter" ]]; then
-    enter="-it"
-    cmd="sh"
+#DKR="24.0.9"
+#DKR="25.0.5"
+DKR="26.1.1"
+ALPINE="3.19"
+
+trap -p stop 1 2 3 6
+
+stop() {
+	docker stop pipenv
+	docker container rm pipenv
+}
+
+#Functions
+digDomains() {
+	if [[ -f domain_list ]]; then
+		n=0
+		for d in $(<domain_list); do
+			((n += 1))
+			printf "%s : %s = %s\n" "${n}" "${d}" "$(dig +short ${d} @${1:-127.1.1.1} | tr '\n' ' ')"
+			sleep ,2
+		done
+	else
+		echo "domain_list not found"
+	fi
+}
+
+if [[ "${1:-''}" == "dig" ]]; then
+	digDomains ${2:-127.1.1.1}
+	exit
+fi
+
+enter=""
+if [[ "${1:-''}" == "enter" ]]; then
+	enter="-it --entrypoint=sh"
 fi
 
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD | sed "s/\//-/g")
 GIT_TAG=$(git describe --tags --exact-match 2>/dev/null || true)
 GIT_TAG="${GIT_TAG:-$GIT_BRANCH}"
+#PLATFORM="linux/arm64"
 PLATFORM="${PLATFORM:-linux/amd64}"
-
 # generate and build dockerfile
-docker buildx build --load --platform=${PLATFORM} --tag image_pipenv --file test/Dockerfile test/
+docker buildx build --load --platform=${PLATFORM} --build-arg alpine_version="${ALPINE}" --build-arg docker_version="${DKR}" --tag image_pipenv --file test/Dockerfile test/
 docker run --rm \
-    --volume /var/run/docker.sock:/var/run/docker.sock \
-    --volume "$(pwd):/$(pwd)" \
-    --workdir "$(pwd)" \
-    --env PIPENV_CACHE_DIR="$(pwd)/.pipenv" \
-    --env GIT_TAG="${GIT_TAG}" \
-    --env PY_COLORS=1 \
-    --env TARGETPLATFORM="${PLATFORM}" \
-    ${enter} image_pipenv ${cmd}
+	--volume /var/run/docker.sock:/var/run/docker.sock \
+	--volume "$(pwd):/$(pwd)" \
+	--workdir "$(pwd)" \
+	--env PIPENV_CACHE_DIR="$(pwd)/.pipenv" \
+	--env GIT_TAG="${GIT_TAG}" \
+	--env PY_COLORS=1 \
+	--env TARGETPLATFORM="${PLATFORM}" \
+	--name pipenv \
+	${enter} image_pipenv
