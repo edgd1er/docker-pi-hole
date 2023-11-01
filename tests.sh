4c216ad21d1aff3b268db238a8f641dd65efca55
edgd1er <3601462+edgd1er@users.noreply.github.com>
2023-11-01 21:09:11 +0100

    fork changes

---
+++ tests.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -eE -u -o pipefail
+
+trap 'printf "\e[31m%s: %s\e[m\n" "Error!" $?' ERR
+#set -x
+GREEN="\e[32m"
+RED="\e[31m"
+NC="\e[0m"
+
+#functions
+buildContainer() {
+  docker compose down -v
+  docker compose up --build -d
+  while [[ 0 -eq $(docker compose -f docker-compose-v6.yml logs | grep -cP '(Container tag is|INFO: Blocking status is enabled)') ]]; do
+    docker compose -f docker-compose-v6.yml logs | tail -5
+    echo "!!!!!!!!! container is not ready !!!!!!!!!!!!!!!!"
+    sleep 5
+  done
+}
+
+checkDns() {
+	testip=$(grep -A1 ports docker-compose-v6.yml | tail -1 | grep -oPm1 "(?<=- \")([0-9\.]+)")
+  dig +short www.free.fr @${testip}
+  [[ $? -eq 0 ]] && echo -e "DNS Resolution ${GREEN}OK${NC}" || echo "${RED}DNS Resolution KO${NC}"
+}
+
+checkAdmin() {
+  testip=$(grep -A1 ports docker-compose-v6.yml | tail -1 | grep -oPm1 "(?<=- \")([0-9\.]+)")
+  res=$(curl -fs "http://${testip}:8053/admin/login.php") || true
+  [[ 0 -le ${#res} ]] && res=$(curl -fs "http://${testip}:8053/admin/") || true
+  [[ 0 -le ${#res} ]] && echo -e "http admin ${GREEN}OK${NC}" || echo -e "${RED}http admin KO ${NC}\n"
+  res=$(curl -fsk "https://${testip}:1443/admin/login.php") || true
+  [[ 0 -le ${#res} ]] && res=$(curl -fsk "https://${testip}:1443/admin/") || true
+  [[ 0 -le ${#res} ]] && echo -e "https admin ${GREEN}OK${NC}" || echo -e "${RED}https admin KO ${NC}\n"
+}
+#Main
+[[ ${1:-''} != "-t" ]] && buildContainer || true
+docker compose logs
+checkDns
+checkAdmin
+[[ ${1:-''} != "-t" ]] && docker compose stop || true
